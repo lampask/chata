497ffae2e6a9664792ce1187c18dd57c6012b406
Lampask <adamgramblicka@hotmail.com>
2020-05-15 12:59:45 +0200

    Constants tweak

--- Assets/Scripts/Globals/Constants.cs
+++ Assets/Scripts/Globals/Constants.cs
@@ -6,6 +6,6 @@ public static class Constants
 {
     public static readonly Vector2 PLAYER_STARTING_POSITION = new Vector2(0, 0);
     public static readonly int CHATA_HEALTH = 100;
-    public static readonly int STARTING_ESSENCE = 10;
+    public static readonly int STARTING_ESSENCE = 20;
     public static readonly int DIGGING_COST = 0;
 }
